0a9632965b6387ee6c0a8914b7cb894952beac39
Ace <subsistence99@gmail.com>
2019-09-01 03:18:46 +0800

    Added helper content.

--- schema/lua/ace/helper.lua
+++ schema/lua/ace/helper.lua
@@ -7,6 +7,10 @@ local function translator(input, seg)
     yield(Candidate("text", seg.start, seg._end, 'Ctrl + C', ' 三重注解'))
     yield(Candidate("text", seg.start, seg._end, 'Ctrl + S', ' 屏蔽词组'))
     yield(Candidate("text", seg.start, seg._end, 'Ctrl + T', ' 显示时钟'))
+    yield(Candidate("text", seg.start, seg._end, '`', ' 双重反查'))
+    yield(Candidate("text", seg.start, seg._end, '`P', ' 全拼反查'))
+    yield(Candidate("text", seg.start, seg._end, '`B', ' 笔画反查'))
+    yield(Candidate("text", seg.start, seg._end, 'env/VAR', ' 环境变量'))
   end
 end
 
