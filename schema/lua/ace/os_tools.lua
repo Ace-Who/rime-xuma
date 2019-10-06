fadb35122fc56dae0ded3865be24ee0241b8c03f
Ace <subsistence99@gmail.com>
2019-10-07 01:40:35 +0800

    New feature: postpone candidates matched by fullcode. Modified or corrected some codes in dict. Enhanced documentation. Many minors improvements and adjustments.

--- schema/lua/ace/os_tools.lua
+++ schema/lua/ace/os_tools.lua
@@ -13,7 +13,7 @@ local function lazy_clock_filter(input, env)
   for cand in input:iter() do
     if page_size > 1 and not done and i == page_size then
       done = true
-      yield(Candidate("time", cand.start, cand._end, os.date("%H:%M:%S"), " 懒钟"))
+      yield(Candidate("lazy_clock", cand.start, cand._end, os.date("%H:%M:%S"), " 懒钟"))
     end
     yield(cand)
     -- Only count unique candidates.
@@ -21,7 +21,7 @@ local function lazy_clock_filter(input, env)
     prev_cand = cand
   end
   if not done then
-    yield(Candidate("time", 1, -1, os.date("%H:%M:%S"), " 不打不走"))
+    yield(Candidate("lazy_clock", 1, -1, os.date("%H:%M:%S"), " 不打不走"))
   end
 end
 
@@ -32,10 +32,19 @@ local function os_env_translator(input, seg)
   if input:find(prefix .. '%w+') then
     local val = os.getenv(input:gsub(prefix, ''))
     if val ~= '' then
-      yield(Candidate("text", seg.start, seg._end, val:sub(1,199), " 环境变量"))
+      yield(Candidate("os_env", seg.start, seg._end, val:sub(1,199), " 环境变量"))
     end
   end
 end
 
+local function os_run_translator(input, seg)
+  local prefix = '^run/'
+  if input:find(prefix .. '%w+') then
+    local command = input:gsub(prefix, '')
+    yield(Candidate("os_run", seg.start, seg._end, command, "运行命令(Ctrl+R)"))
+  end
+end
+
 return { lazy_clock_filter = lazy_clock_filter,
-    os_env_translator = os_env_translator }
+    os_env_translator = os_env_translator,
+    os_run_translator = os_run_translator }
