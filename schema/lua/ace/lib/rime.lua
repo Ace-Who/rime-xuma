645e5177a1994765c9a0577979d71e85d2337b78
Ace <subsistence99@gmail.com>
2020-05-27 12:09:56 +0800

    Modify lua lib script according to functional code changes.

--- schema/lua/ace/lib/rime.lua
+++ schema/lua/ace/lib/rime.lua
@@ -2,18 +2,28 @@
 
 local rime = {}
 package.loaded[...] = rime
-
-
--- 用这样的表描述构词规则，之后解析为另一张表，面向后端函数。彼表格的元素以词组
--- 长度为键，以表格 { char_idx = i, code_idx = j } 的列表为值，其中第 n 个表格
--- 指示词组编码的第 n 码为词组第 char_idx 字的第 code_idx 码。formula 中 'U' 和
--- 'u' 之后的字母表示从末尾倒数，解析为负数索引。
--- sample encoder configuration
--- { { length_equal = 2, formula = 'AaAbBaBb' }
--- , { length_equal = 3, formula = 'AaBaCaCb' }
--- , { length_in_range = {4, 10}, formula = 'AaBaCaZa' }
--- }
 rime.encoder = {}
+
+
+--[[ rime.encoder.load_settings()
+用一个 librime 风格的列表描述构词规则。之后解析为另一张表，面向后端函数，其元素
+以词组长度为键，以表格 { char_idx, code_idx } 的列表为值，依次描述词组的每一码
+。formula 中 'U' 和 'u' 之后的字母表示从末尾倒数，解析为负数索引。
+
+sample encoder configuration (input):
+{ { length_equal = 2, formula = 'AaAbBaBb' }
+, { length_equal = 3, formula = 'AaBaCaCb' }
+, { length_in_range = {4, 10}, formula = 'AaBaCaZa' }
+}
+
+output:
+{ nil
+, { { 1, 1 }, { 1, 2 }, { 2, 1 }, { 2, 2 } }
+, { { 1, 1 }, { 2, 1 }, { 3, 1 }, { 3, 2 } }
+, { { 1, 1 }, { 2, 1 }, { 3, 1 }, { -1, 1 } }
+, ... -- 第 5 至 10 个元素与第 4 个元素相同。
+}
+--]]
 
 
 function rime.encoder.parse_formula(formula)
@@ -24,15 +34,15 @@ function rime.encoder.parse_formula(formula)
     local upper, lower = m:byte(1, 2)
     local char_idx = upper < U and upper - A + 1 or upper - Z - 1
     local code_idx = lower < u and lower - a + 1 or lower - z - 1
-    rule[#rule+1] = { char_idx = char_idx, code_idx = code_idx }
+    rule[#rule+1] = { char_idx, code_idx }
   end
   return rule
 end
 
 
 function rime.encoder.load_settings(setting)
-  local ft, rt = {}, {}
-  local f2r = {}  -- 注意到公式同则规则同，可在 rt 中作引用定义，以节省资源。
+  -- 注意到公式同则规则同，可通过 f2r 在 rt 中作引用定义，以节省资源。
+  local ft, f2r, rt = {}, {}, {}
   for _, t in ipairs(setting) do
     if t.length_equal then ft[t.length_equal] = t.formula
     elseif t.length_in_range then
